02e8134ff433eef60b4bd4c20c93acbe9e40ebf4
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2024-10-29 12:52:51 -0400

    Update NatSpec and harmonize formatting

--- src/MetaVesTFactory.sol
+++ src/MetaVesTFactory.sol
@@ -16,12 +16,13 @@ interface IMetaVesTController {
 /**
  * @title      MetaVesT Factory
  *
- * @notice     Deploy a new instance of MetaVesTController, which in turn deploys a new MetaVesT it controls
+ * @author     MetaLeX Labs, Inc.
+ *
+ * @notice     Factory contract to deploy new instances of MetaVesTController, which may initiate and affect individual MetaVesTs
  *
  **/
 contract MetaVesTFactory {
     event MetaVesT_Deployment(
-        address newMetaVesT,
         address authority,
         address controller,
         address dao,
@@ -32,19 +33,42 @@ contract MetaVesTFactory {
 
     error MetaVesTFactory_ZeroAddress();
 
-    constructor() { }
+    constructor() {}
 
-    /// @notice constructs a MetaVesT framework specifying authority address, DAO staking/voting contract address
-    /// each individual grantee's MetaVesT will be initiated in the newly deployed MetaVesT contract, and deployed MetaVesTs are amendable by 'authority' via the controller contract
-    /// @dev conditionals are contained in the deployed MetaVesT, which is deployed in the MetaVesTController's constructor(); the MetaVesT within the MetaVesTController is immutable, but the 'authority' which has access control within the controller may replace itself
-    /// @param _authority: address which initiates and may update each MetaVesT, such as a BORG or DAO
-    /// @param _dao: contract address which token may be staked and used for voting, typically a DAO pool, governor, staking address. Submit address(0) for no such functionality.
-    function deployMetavestAndController(address _authority, address _dao, address _vestingAllocationFactory, address _tokenOptionFactory, address _restrictedTokenFactory ) external returns(address) {
-        if(_vestingAllocationFactory == address(0) || _tokenOptionFactory == address(0) || _restrictedTokenFactory == address(0))
-           revert MetaVesTFactory_ZeroAddress();
-        metavestController _controller = new metavestController(_authority, _dao, _vestingAllocationFactory, _tokenOptionFactory, _restrictedTokenFactory);
-        emit MetaVesT_Deployment(address(0), _authority, address(_controller), _dao, _vestingAllocationFactory, _tokenOptionFactory, _restrictedTokenFactory);
+    /// @notice constructs a MetaVesT Controller and overall framework specifying authority address, DAO contract address, and each MetaVesT type factory address
+    /// @dev conditionals are contained in the deployed MetaVesT Controller; the `authority` which has access control within the `_controller` may replace itself
+    /// @param _authority address which initiates and may update each MetaVesT
+    /// @param _dao DAO governance contract address which exercises control over ability of 'authority' to call certain functions via imposing conditions in the controller. Submit address(0) for no such functionality.
+    /// @param _vestingFactory vesting allocation factory (VestingAllocationFactory.sol) contract address
+    /// @param _tokenOptionFactory token option factory (TokenOptionFactory.sol) contract address
+    /// @param _restrictedTokenFactory restricted token award factory (RestrictedTokenFactory.sol) contract address
+    function deployMetavestAndController(
+        address _authority,
+        address _dao,
+        address _vestingAllocationFactory,
+        address _tokenOptionFactory,
+        address _restrictedTokenFactory
+    ) external returns (address) {
+        if (
+            _vestingAllocationFactory == address(0) ||
+            _tokenOptionFactory == address(0) ||
+            _restrictedTokenFactory == address(0)
+        ) revert MetaVesTFactory_ZeroAddress();
+        metavestController _controller = new metavestController(
+            _authority,
+            _dao,
+            _vestingAllocationFactory,
+            _tokenOptionFactory,
+            _restrictedTokenFactory
+        );
+        emit MetaVesT_Deployment(
+            _authority,
+            address(_controller),
+            _dao,
+            _vestingAllocationFactory,
+            _tokenOptionFactory,
+            _restrictedTokenFactory
+        );
         return address(_controller);
     }
-
 }
